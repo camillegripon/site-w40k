ec0d444452e88cdb74a1804a63f0f96bb630c09c
camillegripon <camille.gripon@oclock.school>
2025-10-31 16:46:13 +0100

    Ajout Deathwatch

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,16 +1,18 @@
 import '/src/style/header.css';
 import { useState } from 'react';
 
-function Header ({onFactionSelect}) {
+function Header({ onFactionSelect }) {
 
     return <>
-<h1 className="titre">WARHAMMER ARMY BUILDING</h1>
-            <p>Choisissez votre armée</p>
-            <div className="listeLogo">
-                <img src="/logo/logoVotann.png" alt="Ligues des Votanns" className="logo-faction" onClick={() =>onFactionSelect("Votann")}/>
-                <img src="/logo/logoChaos.png" alt="Space marines du chaos" className="logo-faction" onClick={() => onFactionSelect("Marines Du Chaos")}/>
-            </div>
-</>
+        <h1 className="titre">WARHAMMER ARMY BUILDING</h1>
+        <p>Choisissez votre armée</p>
+        <div className="listeLogo">
+            <img src="/logo/logoVotann.png" alt="Ligues des Votanns" className="logo-faction" onClick={() => onFactionSelect("Votann")} />
+            <img src="/logo/logoChaos.png" alt="Space marines du chaos" className="logo-faction" onClick={() => onFactionSelect("Marines Du Chaos")} />
+            <img src="/logo/logoDeathwatch.png" alt="Deathwatch" className="logo-faction" onClick={() => onFactionSelect("Deathwatch")} />
+            <img src="/logo/logoTau.png" alt="Tau" className="logo-faction" onClick={() => onFactionSelect("Tau")} />
+        </div>
+    </>
 
 }
 

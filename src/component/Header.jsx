eab008fe826f57347f07295222d8d5c6e6ff1c77
camillegripon <camille.gripon@oclock.school>
2025-10-31 17:03:22 +0100

    supression de choississez votre armée

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,7 +5,6 @@ function Header({ onFactionSelect }) {
 
     return <>
         <h1 className="titre">WARHAMMER ARMY BUILDING</h1>
-        <p>Choisissez votre armée</p>
         <div className="listeLogo">
             <img src="/logo/logoVotann.png" alt="Ligues des Votanns" className="logo-faction" onClick={() => onFactionSelect("Votann")} />
             <img src="/logo/logoChaos.png" alt="Space marines du chaos" className="logo-faction" onClick={() => onFactionSelect("Marines Du Chaos")} />
